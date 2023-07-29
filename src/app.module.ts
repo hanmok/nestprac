f58bf3a182cbe654d51a01e817b91c1585d1fd61
hanmok <dmammmm@naver.com>
2023-07-29 15:51:44 +0900

    fix wrong driver error

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,67 +1,140 @@
+// import { Module, ValidationPipe, MiddlewareConsumer } from '@nestjs/common';
+// import { APP_PIPE } from '@nestjs/core';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+// import { ConfigModule, ConfigService } from '@nestjs/config';
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
+// import { ReportsModule } from './reports/reports.module';
+// import { UsersModule } from './users/users.module';
+// import { User } from './users/user.entity';
+// import { Report } from './reports/report.entity';
+// const cookieSession = require('cookie-session');
+
+// // @Module({
+// //   imports: [ 
+// //     ConfigModule.forRoot({
+// //       isGlobal: true,
+// //       envFilePath: `.env.${process.env.NODE_ENV}` 
+// //     }),
+// //   TypeOrmModule.forRoot(),
+
+// //     // TypeOrmModule.forRootAsync({
+// //     //   inject: [ConfigService],
+// //     //   useFactory: (config: ConfigService) => { 
+// //     //     return {
+// //     //       type: 'sqlite', 
+// //     //       database: config.get<string>('DB_NAME'),
+// //     //       synchronize: true, 
+// //     //       entities: [User, Report]
+// //     //     }
+// //     //   }
+// //     // }),
+ 
+// //   //   TypeOrmModule.forRoot({
+// //   //   type: 'sqlite',
+// //   //   database: 'db.sqlite',
+// //   //   // database: process.env.NODE_ENV === 'test' ? 'test.sqlite' : 'db.sqlite',
+// //   //   entities: [User, Report],
+// //   //   synchronize: true
+// //   // }),
+
+
+// //     ReportsModule, 
+// //     UsersModule
+// //   ], 
+// //   controllers: [AppController],
+// //   providers: [
+// //     AppService,
+// //     {
+// //       provide: APP_PIPE,
+// //       useValue: new ValidationPipe({
+// //         whitelist: true
+// //       })
+// //     }
+// //   ],
+// // })
+
+
+// @Module({
+//   imports: [
+//     ConfigModule.forRoot({
+//       isGlobal: true,
+//       envFilePath: `.env.${process.env.NODE_ENV}`,
+//     }),
+//     TypeOrmModule.forRoot(),
+//     UsersModule,
+//     ReportsModule,
+//   ],
+//   controllers: [AppController],
+//   providers: [
+//     AppService,
+//     {
+//       provide: APP_PIPE,
+//       useValue: new ValidationPipe({
+//         whitelist: true,
+//       }),
+//     },
+//   ],
+// })
+
+
+// export class AppModule {
+
+//   constructor(private configService: ConfigService) {}
+
+//   configure(consumer: MiddlewareConsumer) { 
+//     consumer.apply(cookieSession({
+//       keys: [this.configService.get('COOKIE_KEY')],
+      
+//     })).forRoutes('*');
+//   }
+// }
+
+
 import { Module, ValidationPipe, MiddlewareConsumer } from '@nestjs/common';
 import { APP_PIPE } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ReportsModule } from './reports/reports.module';
 import { UsersModule } from './users/users.module';
+import { ReportsModule } from './reports/reports.module';
 import { User } from './users/user.entity';
 import { Report } from './reports/report.entity';
 const cookieSession = require('cookie-session');
+const dbConfig = require('../ormconfig');
 
 @Module({
-  imports: [ 
+  imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: `.env.${process.env.NODE_ENV}` 
+      envFilePath: `.env.${process.env.NODE_ENV}`,
     }),
-
-    TypeOrmModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => { 
-        return {
-          type: 'sqlite', 
-          database: config.get<string>('DB_NAME'),
-          synchronize: true, 
-          entities: [User, Report]
-        }
-      }
-    }),
-
-  //   TypeOrmModule.forRoot({
-  //   type: 'sqlite',
-  //   database: 'db.sqlite',
-  //   // database: process.env.NODE_ENV === 'test' ? 'test.sqlite' : 'db.sqlite',
-  //   entities: [User, Report],
-  //   synchronize: true
-  // }),
-
-
-    ReportsModule, 
-    UsersModule
-  ], 
+    TypeOrmModule.forRoot(dbConfig),
+    UsersModule,
+    ReportsModule,
+  ],
   controllers: [AppController],
   providers: [
     AppService,
     {
       provide: APP_PIPE,
       useValue: new ValidationPipe({
-        whitelist: true
-      })
-    }
+        whitelist: true,
+      }),
+    },
   ],
 })
-
-
 export class AppModule {
-
   constructor(private configService: ConfigService) {}
 
-  configure(consumer: MiddlewareConsumer) { 
-    consumer.apply(cookieSession({
-      keys: [this.configService.get('COOKIE_KEY')],
-      
-    })).forRoutes('*');
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(
+        cookieSession({
+          keys: [this.configService.get('COOKIE_KEY')],
+        }),
+      )
+      .forRoutes('*');
   }
 }
